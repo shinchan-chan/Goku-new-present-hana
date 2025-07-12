9b4fb81b77d546cd80cad39b89db5acc189a7be7
shinchan-chan <shinchanadamopyt21@gmail.com>
2025-07-12 11:49:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Click
 START_MSG = os.environ.get("START_MESSAGE", "<b>ʜɪ ᴛʜᴇʀᴇ... {first}! 💥\n\nɪ ᴀᴍ ᴀ ꜰɪʟᴇ ꜱᴛᴏʀᴇ ʙᴏᴛ...!\nɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴘʀɪᴠᴀᴛᴇ ꜰɪʟᴇꜱ ᴛʜʀᴏᴜɢʜ ᴀ ꜱᴘᴇᴄɪꜰɪᴄ ʟɪɴᴋ....!\n\nᴅᴇᴠᴇʟᴏᴘᴇᴅ ғᴏʀ : [ @Adult_Flux ] </b>")
 try:
     ADMINS=[7827448605]
-    for x in (os.environ.get("ADMINS", "1683225887 7827448605 8160777407").split()):
+    for x in (os.environ.get("ADMINS", "1683225887 7827448605 5195665501").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
